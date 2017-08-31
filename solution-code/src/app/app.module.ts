987beffbdb6e341de858b2a84c9583206766775c
ZebGirouard <timzebgir@gmail.com>
2017-08-31 10:52:26 -0600

    Bringing in latest from global

--- solution-code/src/app/app.module.ts
+++ solution-code/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { InternalDefensesModule } from './internal-defenses/internal-defenses.module'
+import { InternalDefensesModule } from './internal-defenses/internal-defenses.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
